dc9286653f1804a184dcad41f83dfb3af91ca286
vijayB725 <kjvj725@gmail.com>
2020-08-01 18:38:04 +0530

    2 files modified, 1 added

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,12 @@
+class employee{
+  id: number;
+  name: string;
+  gender: string;
+  email? : string;
+  phoneNumber?: number;
+  contactPreference: string;
+  dateOfBirth : string;
+  department: string;
+  isActive: boolean;
+  photopath?: string 
+}
