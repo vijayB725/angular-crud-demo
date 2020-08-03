750a57fd25a3876a0315ec623b6b1ddcbe997762
vijayB725 <kjvj725@gmail.com>
2020-08-03 13:20:45 +0530

    1 file modified

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { employee } from '../employee.model';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  employees : employee[];
+  constructor() { 
+    this.employees = emplist;
+    console.log(emplist);
+  }
+  
 
   ngOnInit() {
   }
 
 }
+
+ const emplist = [
+  {
+  id: 101,
+  name: "Mark",
+  gender: "male",
+  email : "mark@gmail.com",
+  phoneNumber: 4563423485,
+  contactPreference: "phone",
+  dateOfBirth : "04-12-1996",
+  department: "sales",
+  isActive: true,
+  photopath: "assets/images/b1.jpeg" 
+  }
+];
